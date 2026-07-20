c1dd9ea6339457e7b4c4e4a8a683a1597af68f63
Dwann Williams <dwann.williams21@stu-mail.ccny.cuny.edu>
2026-07-20 11:00:58 -0400

    Fixed issue where changing account name made a new account with that name, rather than just changing the current accounts name

--- src/accounts/AccountService.java
+++ src/accounts/AccountService.java
@@ -145,7 +145,7 @@ import java.security.NoSuchAlgorithmException;
 			Account currentUser = SessionManager.getCurrentUser();
 			if (currentUser != null){
 				try{
-					AccountFileManager.saveAccount(currentUser);
+					AccountFileManager.saveAccount(currentUser, currentUser.getUsername());
 					System.out.println("Account saved for "+ currentUser.getUsername());
 				}
 				catch (Exception e){
@@ -213,7 +213,7 @@ import java.security.NoSuchAlgorithmException;
 		
 		account.setSecretQuestion(question);
 		account.setSecretAnswer(hash(answer));
-		AccountFileManager.saveAccount(account);
+		AccountFileManager.saveAccount(account, account.getUsername());
 		return true;
 	}
 	
@@ -300,7 +300,7 @@ import java.security.NoSuchAlgorithmException;
 		}
 		
 		account.setHashedPassword(hash(newPassword));
-		AccountFileManager.saveAccount(account);
+		AccountFileManager.saveAccount(account, account.getUsername());
 		return true;
 	}
 	
@@ -340,7 +340,7 @@ import java.security.NoSuchAlgorithmException;
 		
 		// Update the account with the hashed password
 		account.setHashedPassword(hashedPassword);
-		AccountFileManager.saveAccount(account);
+		AccountFileManager.saveAccount(account, account.getUsername());
 		return true;
 	}
 	
@@ -362,8 +362,9 @@ import java.security.NoSuchAlgorithmException;
 		} else if (AccountFileManager.accountExists(newUsername)) {
 			return false; 
 		} else {
+			String oldUsername = account.getUsername();
 			account.setUsername(newUsername);
-			AccountFileManager.saveAccount(account);
+			AccountFileManager.saveAccount(account, oldUsername);
 			return true;
 		}
 	}
